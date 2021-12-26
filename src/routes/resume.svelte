9129e47c7ca51b559b9f10653e917f9c7acfc828
Liang Jun <phualiangjun@gmail.com>
2021-12-26 22:05:19 +0800

    base template with lorem

--- src/routes/resume.svelte
+++ src/routes/resume.svelte
@@ -1,9 +1,91 @@
 <main
-	class="p-4 mx-auto md:max-w-screen-lg grid grid-areas-resume-slim grid-cols-1 md:grid-areas-resume md:grid-cols-resume md:grid-rows-resume"
+	class="p-4 mx-auto lg:max-w-screen-lg grid grid-areas-resume-slim grid-cols-1 lg:grid-areas-resume lg:grid-cols-resume lg:grid-rows-resume"
 >
-	<section class="grid-in-header">LJ</section>
-	<section class="grid-in-introduction">intro</section>
-	<section class="grid-in-skills">skills</section>
-	<section class="grid-in-education">education</section>
-	<section class="grid-in-experience">experience</section>
+	<section class="grid-in-header flex flex-col lg:items-end p-4 border-b">
+		<h1 class="text-3xl font-bold mb-2 mt-auto">
+			<span class="underline">Liang Jun</span> Phua
+		</h1>
+		<h6 class="text-grey font-mono">Fullstack Developer</h6>
+		<h6 class="text-grey font-mono">Scrum Master</h6>
+	</section>
+	<section class="grid-in-introduction flex flex-col p-4 border-b lg:border-l">
+		<h3 class="font-semibold">introduction</h3>
+		<p>
+			Quis exercitation occaecat fugiat reprehenderit sunt adipisicing sit laboris esse et aliqua
+			irure. Dolor dolor nulla consequat laboris ea. Fugiat veniam amet eu deserunt pariatur aliquip
+			officia ea incididunt nulla proident. Veniam tempor ullamco fugiat dolore laboris eiusmod
+			dolor velit consectetur non labore labore sit et. Dolor aute excepteur voluptate officia
+			dolore commodo qui ad commodo dolore reprehenderit amet minim in. Cillum dolor Lorem nisi nisi
+			enim laborum pariatur id mollit sint exercitation eu anim.
+		</p>
+	</section>
+	<section class="grid-in-contact flex flex-col lg:items-end p-4 border-b">
+		<h3 class="font-semibold">contact</h3>
+		<p class="lg:text-right">
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate numquam culpa, iusto
+			sequi nobis consectetur a ad ab ipsa quas praesentium nisi dolor ut quibusdam, delectus et
+			perspiciatis aperiam natus.
+		</p>
+	</section>
+	<section class="grid-in-skills flex flex-col lg:items-end p-4 border-b">
+		<h3 class="font-semibold">skills stack</h3>
+		<p class="lg:text-right">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores autem explicabo commodi animi
+			suscipit dolores cum laborum? At suscipit doloribus qui id officia. Nemo, veniam quibusdam
+			dolores ad cum culpa?
+		</p>
+	</section>
+	<section class="grid-in-education flex flex-col lg:items-end p-4 border-b">
+		<h3 class="font-semibold">education</h3>
+		<p class="lg:text-right">
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, debitis! Provident accusamus,
+			alias eos voluptate porro fuga laudantium reiciendis, tenetur vero doloribus nesciunt officia voluptates
+			perferendis reprehenderit! Soluta, nisi non?
+		</p>
+	</section>
+	<section class="grid-in-experience flex flex-col p-4 lg:border-l">
+		<h3 class="font-semibold">experiences</h3>
+		<div>
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi aspernatur ipsa
+			atque, natus reprehenderit mollitia explicabo! Temporibus quae, ullam, autem eligendi quis
+			porro, soluta quia unde obcaecati recusandae officia.
+		</div>
+		<br />
+		<div>
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi aspernatur ipsa
+			atque, natus reprehenderit mollitia explicabo! Temporibus quae, ullam, autem eligendi quis
+			porro, soluta quia unde obcaecati recusandae officia.
+		</div>
+		<br />
+		<div>
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi aspernatur ipsa
+			atque, natus reprehenderit mollitia explicabo! Temporibus quae, ullam, autem eligendi quis
+			porro, soluta quia unde obcaecati recusandae officia.
+		</div>
+		<br />
+		<div>
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi aspernatur ipsa
+			atque, natus reprehenderit mollitia explicabo! Temporibus quae, ullam, autem eligendi quis
+			porro, soluta quia unde obcaecati recusandae officia.
+		</div>
+		<br />
+		<div>
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi aspernatur ipsa
+			atque, natus reprehenderit mollitia explicabo! Temporibus quae, ullam, autem eligendi quis
+			porro, soluta quia unde obcaecati recusandae officia.
+		</div>
+		<br />
+		<div>
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi aspernatur ipsa
+			atque, natus reprehenderit mollitia explicabo! Temporibus quae, ullam, autem eligendi quis
+			porro, soluta quia unde obcaecati recusandae officia.
+		</div>
+		<br />
+		<div>
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis excepturi aspernatur ipsa
+			atque, natus reprehenderit mollitia explicabo! Temporibus quae, ullam, autem eligendi quis
+			porro, soluta quia unde obcaecati recusandae officia.
+		</div>
+	</section>
+	<!-- <div class="grid-in-space p-4 text-transparent">secret text</div> -->
 </main>
