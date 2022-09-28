8674bf471d59fd8755477dda39ca76ec92b03fe2
lj <phualiangjun@gmail.com>
2022-09-28 16:38:00 +0800

    fix  fontawesome css issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import '../node_modules/devicon/devicon.min.css'
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import type { AppProps } from "next/app";
+import "../node_modules/devicon/devicon.min.css";
+import "../styles/globals.css";
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
